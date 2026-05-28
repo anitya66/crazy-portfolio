629d41ea2abfbcc084754edacc639d7518bf0253
anitya66 <123goldirajput@gmail.com>
2026-05-28 19:26:21 +0530

    fixed real live visitor counter

--- src/components/VisitorCounter.jsx
+++ src/components/VisitorCounter.jsx
@@ -1,16 +1,55 @@
+import { useEffect, useState }
+
+from "react"
+
 function VisitorCounter(){
+
+const[
+count,
+setCount
+]=useState(0)
+
+useEffect(()=>{
+
+fetch(
+
+"https://api.countapi.xyz/hit/crazy-portfolio/visitor"
+
+)
+
+.then(
+(res)=>res.json()
+)
+
+.then(
+(data)=>{
+
+setCount(data.value)
+
+}
+)
+
+.catch(
+(err)=>console.log(err)
+)
+
+},[])
 
 return(
 
 <div className="visitor">
 
-<img
+<span>
 
-src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fcrazy-portfolio-chi.vercel.app%2F&countColor=%238B5CF6"
+👁
 
-alt="visitor counter"
+</span>
 
-/>
+<p>
+
+{count}
+
+</p>
 
 </div>
 
