0c827e27497426bc7d25e79fe5e3143c486c9e43
anitya66 <123goldirajput@gmail.com>
2026-05-28 19:22:17 +0530

    fixed real live visitor counter

--- src/components/VisitorCounter.jsx
+++ src/components/VisitorCounter.jsx
@@ -6,9 +6,9 @@ return(
 
 <img
 
-src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fcrazy-portfolio-chi.vercel.app%2F&label=Views&countColor=%238B5CF6"
+src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fcrazy-portfolio-chi.vercel.app%2F&countColor=%238B5CF6"
 
-alt="visitor badge"
+alt="visitor counter"
 
 />
 
